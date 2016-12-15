60fa2017c21fd9e5b2be90f600b45d07e75982fe
brad_rust <brad.rust@uky.edu>
2016-12-15 18:42:15 -0500

    Passes test vendingMachineDisplaysAmountOfLastCoinInserted for trivial implementation

---
+++ VendingMachine_Kata/VendingMachine_Kata/VendingMachine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine_Kata
+{
+    public class VendingMachine
+    {
+        public double credit {private set; get;}
+
+        public void addCoin(string coin)
+        {
+            this.credit = .10;
+        }
+    }
+}
